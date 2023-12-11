b0b867b725f11edb930682e917236beb5b6a7cd9
Kirill <kirillteplycov888@gmail.com>
2023-12-11 16:07:00 +0500

    Разработанные интерфейсы. Тепляков

---
+++ Shop_Тепляков/Interfaces/IContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Тепляков.Interfaces
+{
+    public interface IContext
+    {
+        List<object> All();
+        void Save(bool Update = false);
+        void Delete();
+    }
+}
